029784ae3e9d87bd877c91a4c23401f4172a94ca
BibianaBalBar <balbuenabibi@gmail.com>
2020-07-11 00:37:11 +0100

    sass organized

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const nav = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
 const links = nav.querySelectorAll("a");
+const cloud1 = document.querySelector(".moving-cloud-1");
+const cloud2 = document.querySelector(".moving-cloud-2");
 
 burger.addEventListener("click", () => {
     nav.classList.toggle("nav-open");
     burger.classList.toggle("toggle");
+    cloud1.classList.toggle("cloud-burger");
+    cloud2.classList.toggle("cloud-burger");
 });
 
 links.forEach(link => {
