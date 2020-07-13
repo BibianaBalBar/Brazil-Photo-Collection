ee365a18ab6c86d712d5fe796c4e6d05973ff125
BibianaBalBar <balbuenabibi@gmail.com>
2020-07-13 23:13:16 +0100

    new grid and hover effect

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// burger
+
 const nav = document.querySelector(".nav-links");
 const burger = document.querySelector(".burger");
 const links = nav.querySelectorAll("a");
@@ -17,3 +19,29 @@ links.forEach(link => {
         burger.classList.toggle("toggle");
     });
 });
+
+
+// pop up
+
+document.addEventListener("DOMContentLoaded", function(){
+    let thumbnails = document.querySelectorAll(".image-popup");
+    let popupBackground = document.querySelector("#popup-background");
+    let popupTitle = document.querySelector("#popup-title");
+    let popupImage = document.querySelector("#popup-image");
+
+    for (let i = 0; i < thumbnails.length; i++) {
+        thumbnails[i].addEventListener("click", function(){
+            popupBackground.style.display = "block";
+            popupTitle.innerHTML = this.alt;
+            popupImage.src = this.src;
+        })
+    }
+    popupBackground.addEventListener("click", function(){
+        popupBackground.style.display = "none";
+    })
+    document.addEventListener("keydown", function(e){
+        if (e.keyCode == 27) {
+            popupBackground.style.display = "none";
+        }
+    })
+})
